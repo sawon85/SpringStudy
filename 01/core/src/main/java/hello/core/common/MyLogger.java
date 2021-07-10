032f2d68a301caa5007ea3dbe94f688d9ca321d9
sawon choi <sawon49@naver.com>
2021-07-11 00:22:28 +0900

    web scope study

--- 01/core/src/main/java/hello/core/common/MyLogger.java
+++ 01/core/src/main/java/hello/core/common/MyLogger.java
@@ -3,7 +3,10 @@ package hello.core.common;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.sql.SQLOutput;
+import java.util.UUID;
 
 @Component
 @Scope(value = "request")
@@ -18,6 +21,17 @@ public class MyLogger {
 
 
     public void log(String message) {
-        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init(){
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
     }
 }
