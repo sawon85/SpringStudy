94d9c997a41f8f25a4d9deec18d66b1d7384ed7a
sawon choi <sawon49@naver.com>
2021-07-13 23:03:20 +0900

    mvc template engine study

--- 00/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ 00/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -17,8 +17,8 @@ public class HelloController {
 
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
-
         model.addAttribute("name", name);
+        
         return "hello-template";
     }
 
